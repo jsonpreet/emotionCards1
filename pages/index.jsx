00ba218efd30328241eea6fab7eb77f0452a5d15
MrPreet <harpreet9629@gmail.com>
2022-10-08 00:23:40 +0530

    supabase updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,7 +18,7 @@ export default function Home ({user}) {
         <title>Dashboard - Emotion Cards</title>
       </Head>
       <Layout>
-        <Dashboard/>
+        {/* <Dashboard/> */}
       </Layout>
     </>
   )
